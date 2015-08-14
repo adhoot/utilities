97a74682e8118616f95e564e2d3677dd05f82429
Anubhav Dhoot <adhoot@cloudera.com>
2015-08-13 19:00:12 -0700

    pass in user and protect overwriting files

--- src/uploadprivates.py
+++ src/uploadprivates.py
@@ -7,16 +7,18 @@ def main(argv):
   jar = 'hadoop-mapreduce-client-core-2.2.0-cdh5.0.0-beta-2-SNAPSHOT.jar'
   dir = '/opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/'
   privatesdir = "/Users/adhoot/privates/"
+  user = "adhoot"
 
   try:
-    opts, args = getopt.getopt(argv,"hn:j:d:",["nodelist=","jar=","dir="])
-  except getopt.GetoptError:
-    print 'uploadprivates.py -n <nodelist file> -j <jar file> -d <dir to place jar>  -p <dir containing privates'
+    opts, args = getopt.getopt(argv,"hn:j:d:p:u:",["nodelist=","jar=","dir=","privatesdir=","user="])
+  except getopt.GetoptError as err:
+    print(err)
+    print 'uploadprivates.py -n <nodelist file> -j <jar file> -d <dir to place jar>  -p <dir containing privates> -u <user for ssh>'
     sys.exit(2)
 
   for opt, arg in opts:
     if opt == '-h':
-      print 'uploadprivates.py -n <nodelist file> -j <jar file> -d <dir to place jar> -p <dir containing privates>'
+      print 'uploadprivates.py -n <nodelist file> -j <jar file> -d <dir to place jar> -p <dir containing privates> -u <user for ssh>'
       sys.exit()
     elif opt in ("-n", "--nodelist"):
       nodeslist = arg
@@ -26,6 +28,8 @@ def main(argv):
       dir = arg
     elif opt in ("-p", "--privatesdir"):
       privatesdir = arg
+    elif opt in ("-u", "--user"):
+      user = arg
 
   print "nodelist = {0}, jar = {1}, destination dir = {2}, privates dir = {3}".format(nodeslist, jar, dir, privatesdir)
 
@@ -34,7 +38,7 @@ def main(argv):
           machineName = m.strip()
           print "Upload privates to " + machineName
           jarName = jar.strip()
-          cmd = "rsync -ave ssh {2}{1} root@{0}:~/jars/".format(machineName, jarName, privatesdir)
+          cmd = "rsync -ave ssh  {2}{1} {3}@{0}:~/jars/".format(machineName, jarName, privatesdir, user)
           print cmd
           os.system(cmd)
 
@@ -43,11 +47,11 @@ def main(argv):
 
           print "Copy privates on " + machineName
           cmd = (
-          'ssh root@{0} "ls -l {2}{1}*'
-                 + ' ;sudo mv {2}{1}  {2}{3}'
-                 + ' ;sudo cp ~/jars/{1} {2}/{1}'
+          'ssh  {4}@{0} "ls -l {2}{1}*'
+                 + ' ;sudo mv -n  {2}{1}  {2}{3}'
+                 + ' ;sudo cp -n ~/jars/{1} {2}/{1}'
                  + ' ;ls -l {2}{1}*'
-                 +'"').format(machineName, jarName, dir, backupName)
+                 +'"').format(machineName, jarName, dir, backupName, user)
           print cmd
           os.system(cmd)
 
